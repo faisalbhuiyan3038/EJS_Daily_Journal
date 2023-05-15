19d6c4bc47e35312c5a817cc3b50b821c6b8b1d9
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-05-15 20:55:35 +0600

    added compose page and fixed routes

--- app.js
+++ app.js
@@ -15,15 +15,35 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req,res){
-  res.render("home", {startingContent: homeStartingContent});
+  res.render("home", {startingContent: homeStartingContent, posts: posts});
 });
 
+app.get("/about", function(req,res){
+  res.render("about", {aboutContentvar: aboutContent});
+});
 
+app.get("/contact", function(req,res){
+  res.render("contact", {contactContentvar: contactContent});
+});
 
+app.get("/compose", function(req,res){
+  res.render("compose");
+});
 
+app.post("/compose", function(req,res){
 
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  }
 
+  posts.push(post);
+
+  res.redirect("/");
+});
 
 
 
